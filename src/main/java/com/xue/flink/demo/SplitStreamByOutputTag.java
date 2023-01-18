d7497a4b82af0d5d231a0c9101fc6750a64ca326
“xuexiong” <xuexiong@souche.com>
2023-01-18 10:09:26 +0800

    feat:增加合流分流例子

---
+++ src/main/java/com/xue/flink/demo/SplitStreamByOutputTag.java
@@ -0,0 +1,59 @@
+package com.xue.flink.demo;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.OutputTag;
+
+/**
+ * 分流的例子
+ */
+public class SplitStreamByOutputTag {
+    // 定义输出标签，侧输出流的数据类型为三元组(user, url, timestamp)
+    private static OutputTag<Tuple3<String, String, Long>> MaryTag = new
+            OutputTag<Tuple3<String, String, Long>>("Mary-pv") {
+            };
+    private static OutputTag<Tuple3<String, String, Long>> BobTag = new
+            OutputTag<Tuple3<String, String, Long>>("Bob-pv") {
+            };
+
+    public static void main(String[] args) throws Exception {
+        /*
+        定义环境
+         */
+        StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        /*
+        添加数据源
+         */
+        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource());
+
+        /*
+        通过侧输出流 分隔 不通的输出流 用标签来区分
+         */
+        SingleOutputStreamOperator<Event> processedStream
+                = stream.process(new ProcessFunction<Event, Event>() {
+            @Override
+            public void processElement(Event value, Context ctx, Collector<Event> out) throws Exception {
+                if (value.user.equals("Mary")) {
+                    // Mary 标签输出流
+                    ctx.output(MaryTag, new Tuple3<>(value.user, value.url,
+                            value.timestamp));
+                } else if (value.user.equals("Bob")) {
+                    // Bob 标签输出流
+                    ctx.output(BobTag, new Tuple3<>(value.user, value.url,
+                            value.timestamp));
+                } else {
+                    out.collect(value);
+                }
+            }
+        });
+        processedStream.getSideOutput(MaryTag).print("Mary pv");
+        processedStream.getSideOutput(BobTag).print("Bob pv");
+        processedStream.print("else");
+        env.execute();
+    }
+}
